5a8d9536713d1aeca8db4ac15fd672aaffee84d0
Ryan Foster <RytoEX@gmail.com>
2021-09-13 23:59:23 -0400

    update nasm to 2.15.05

--- win-install-tools.sh
+++ win-install-tools.sh
@@ -3,10 +3,9 @@
 sudo apt install automake cmake curl git libtool mingw-w64 mingw-w64-tools \
 	pkg-config wget
 
-curl -L -O http://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.xz
-tar -xf nasm-2.14.02.tar.xz
-cd ./nasm-2.14.02
+curl -L -O http://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.xz
+tar -xf nasm-2.15.05.tar.xz
+cd ./nasm-2.15.05
 ./configure
 make
 sudo make install
-
