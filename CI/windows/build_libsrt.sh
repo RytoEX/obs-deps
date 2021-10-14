48f1c2233652d90ecddd11ee3b2737e599ae7880
Ryan Foster <RytoEX@gmail.com>
2021-10-14 02:10:20 -0400

    build libsrt

--- CI/windows/build_libsrt.sh
+++ CI/windows/build_libsrt.sh
@@ -1,41 +1,56 @@
 #!/bin/bash
 
-##############################################################################
+################################################################################
 # Windows libsrt cross-compile build script
-##############################################################################
+################################################################################
 #
-# This script file can be included in build scripts for macOS or run directly
+# This script file can be included in build scripts for Windows or run directly
 #
-##############################################################################
+################################################################################
 
 # Halt on errors
 set -eE
 
-_build_product() {
+_patch_product() {
     cd "${PRODUCT_FOLDER}"
 
+    step "Apply patches..."
+    apply_patch "${CHECKOUT_DIR}/CI/windows/patches/libsrt/srt-minsizerel.patch" "e586ba574c0f3a8468135be7703f287272e2000670c6e926247e8178af237366"
+}
+
+_build_product() {
+    ensure_dir "${PRODUCT_FOLDER}/build_${ARCH}"
+
     step "Configure (${ARCH})..."
-    cmake -S . -B build_${ARCH} -G Ninja ${CMAKE_CCACHE_OPTIONS} \
+    cmake .. ${CMAKE_CCACHE_OPTIONS} \
+        -DCMAKE_SYSTEM_NAME=Windows \
+        -DCMAKE_C_COMPILER=$WIN_CROSS_TOOL_PREFIX-w64-mingw32-gcc \
+        -DCMAKE_CXX_COMPILER=$WIN_CROSS_TOOL_PREFIX-w64-mingw32-g++ \
         -DCMAKE_INSTALL_PREFIX="${BUILD_DIR}" \
-        -DCMAKE_OSX_ARCHITECTURES="${CMAKE_ARCHS}" \
-        -DENABLE_APPS=OFF \
+        -DCMAKE_RC_COMPILER=$WIN_CROSS_TOOL_PREFIX-w64-mingw32-windres \
         -DUSE_ENCLIB="mbedtls" \
-        -DENABLE_STATIC=ON \
-        -DENABLE_SHARED=OFF \
-        -DSSL_INCLUDE_DIRS="${BUILD_DIR}/include" \
-        -DSSL_LIBRARY_DIRS="${BUILD_DIR}/lib" \
-        -DCMAKE_FIND_FRAMEWORK="LAST" \
+        -DENABLE_APPS=OFF \
+        -DENABLE_STATIC=OFF \
+        -DENABLE_SHARED=ON \
+        -DCMAKE_C_FLAGS="-I${CHECKOUT_DIR}/windows_build_temp/pthread-win32" \
+        -DCMAKE_CXX_FLAGS="-I${CHECKOUT_DIR}/windows_build_temp/pthread-win32" \
+        -DCMAKE_SHARED_LINKER_FLAGS="-static-libgcc -Wl,--strip-debug" \
+        -DPTHREAD_LIBRARY="${BUILD_DIR}/lib/libpthreadGC2.a" \
+        -DPTHREAD_INCLUDE_DIR="${CHECKOUT_DIR}/windows_build_temp/pthread-win32" \
+        -DUSE_OPENSSL_PC=OFF \
+        -DCMAKE_BUILD_TYPE=MinSizeRel \
         ${QUIET:+-Wno-deprecated -Wno-dev --log-level=ERROR}
 
     step "Build (${ARCH})..."
-    cmake --build build_${ARCH} --config "Release"
+    make -j$PARALLELISM
+    $WIN_CROSS_TOOL_PREFIX-w64-mingw32-strip -w --keep-symbol=srt* libsrt.dll
 }
 
 _install_product() {
-    cd "${PRODUCT_FOLDER}"
+    cd "${PRODUCT_FOLDER}/build_${ARCH}"
 
     step "Install (${ARCH})..."
-    cmake --install build_${ARCH} --config "Release"
+    make install
 }
 
 build-libsrt-main() {
@@ -44,7 +59,7 @@ build-libsrt-main() {
     if [ -z "${_RUN_OBS_BUILD_SCRIPT}" ]; then
         CHECKOUT_DIR="$(/usr/bin/git rev-parse --show-toplevel)"
         source "${CHECKOUT_DIR}/CI/include/build_support.sh"
-        source "${CHECKOUT_DIR}/CI/include/build_support_macos.sh"
+        source "${CHECKOUT_DIR}/CI/include/build_support_windows_cross.sh"
 
         _check_parameters $*
         _build_checks
