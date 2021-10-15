545eaf082c1b3eeec3c0a7d751aa771fa2e63c97
Ryan Foster <RytoEX@gmail.com>
2021-10-15 05:40:52 -0400

    build nasm in install_tools

--- CI/include/build_support_windows_cross.sh
+++ CI/include/build_support_windows_cross.sh
@@ -29,6 +29,13 @@ fi
 ## DEFINE UTILITIES ##
 install_tools() {
     sudo apt ${QUIET:+--quiet} -y install automake cmake curl git libtool mingw-w64 mingw-w64-tools pkg-config wget
+
+    curl -L -O https://www.nasm.us/pub/nasm/releasebuilds/2.15.01/nasm-2.15.01.tar.xz
+    tar -xf nasm-2.15.01.tar.xz
+    cd ./nasm-2.15.01
+    ./configure
+    make
+    sudo make install
 }
 
 check_curl() {
