1accefe0af9484081e28604b0b9561e44f4cb7a4
Ryan Foster <RytoEX@gmail.com>
2021-10-15 06:17:34 -0400

    build libpng

--- CI/windows/build_libpng.sh
+++ CI/windows/build_libpng.sh
@@ -1,49 +1,37 @@
 #!/bin/bash
 
-##############################################################################
-# macOS libpng build script
-##############################################################################
+################################################################################
+# Windows libpng cross-compile build script
+################################################################################
 #
-# This script file can be included in build scripts for macOS or run directly
+# This script file can be included in build scripts for Windows or run directly
 #
-##############################################################################
+################################################################################
 
 # Halt on errors
 set -eE
-
-_patch_product() {
-    cd "${PRODUCT_FOLDER}"
-
-    step "Apply patches..."
-    apply_patch "${CHECKOUT_DIR}/CI/macos/patches/libpng.patch" "c6d7c7ca5ca016c839fd2579ac8ab46e2e3940d4027166e9315797b146918f54"
-}
 
 _build_product() {
     cd ${PRODUCT_FOLDER}
 
     step "Configure ("${ARCH}")..."
-    cmake -S . -B build_${ARCH} -G Ninja ${CMAKE_CCACHE_OPTIONS} \
-        -DCMAKE_INSTALL_PREFIX="${BUILD_DIR}" \
-        -DCMAKE_PREFIX_PATH="${BUILD_DIR}" \
-        -DCMAKE_OSX_ARCHITECTURES="${CMAKE_ARCHS}" \
-        -DPNG_SHARED=OFF \
-        -DPNG_TESTS=OFF \
-        -DPNG_STATIC=ON \
-        -DPNG_ARM_NEON=on \
-        -DCMAKE_ASM_FLAGS="-DPNG_ARM_NEON_IMPLEMENTATION=1" \
-        ${QUIET:+-Wno-deprecated -Wno-dev --log-level=ERROR}
-
-    mkdir -p build_${ARCH}/arm64
+    PKG_CONFIG_PATH="${BUILD_DIR}/lib/pkgconfig" \
+        LDFLAGS="-L${BUILD_DIR}/lib -static-libgcc" \
+        CPPFLAGS="-I${BUILD_DIR}/include" \
+        ./configure \
+        --host=$WIN_CROSS_TOOL_PREFIX-w64-mingw32 \
+        --prefix="${BUILD_DIR}" \
+        --enable-shared
 
     step "Compile ("${ARCH}")..."
-    cmake --build build_${ARCH} --config "Release"
+    make -j$PARALLELISM
 }
 
 _install_product() {
     cd "${PRODUCT_FOLDER}"
 
     step "Install ("${ARCH}").."
-    cmake --install build_${ARCH} --config "Release"
+    make install
 }
 
 build-libpng-main() {
@@ -52,7 +40,7 @@ build-libpng-main() {
     if [ -z "${_RUN_OBS_BUILD_SCRIPT}" ]; then
         CHECKOUT_DIR="$(/usr/bin/git rev-parse --show-toplevel)"
         source "${CHECKOUT_DIR}/CI/include/build_support.sh"
-        source "${CHECKOUT_DIR}/CI/include/build_support_macos.sh"
+        source "${CHECKOUT_DIR}/CI/include/build_support_windows_cross.sh"
         _check_parameters $*
         _build_checks
     fi
