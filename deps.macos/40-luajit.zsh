e9fcfbb67e801777dcd3cb275720225144131b6c
Ryan Foster <ryan@obsproject.com>
2025-12-23 15:15:47 -0500

    deps.macos: Update LuaJIT to v2.1 7152e1548

--- deps.macos/40-luajit.zsh
+++ deps.macos/40-luajit.zsh
@@ -4,7 +4,7 @@ autoload -Uz log_debug log_error log_info log_status log_output
 local name='luajit'
 local version='2.1'
 local url='https://github.com/LuaJIT/LuaJIT.git'
-local hash='a4f56a459a588ae768801074b46ba0adcfb49eb1'
+local hash='7152e15489d2077cd299ee23e3d51a4c599ab14f'
 local branch='v2.1'
 local patches=(
   "${0:a:h}/patches/libluajit/0001-change-hardcoded-compiler-name.patch \
